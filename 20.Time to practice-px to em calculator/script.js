0bd31b1eb74ad9c4d203113f067144a0076e6ed3
NanaYamamoto <chyhyy76@gmail.com>
2024-01-16 22:16:55 +0900

    add mini js project

--- 20.Time to practice-px to em calculator/script.js
+++ 20.Time to practice-px to em calculator/script.js
@@ -1,1 +1,22 @@
 // em = required / parent 
+//var initialResult = document.getElementById('result');
+//console.log(initialResult);
+
+var button = document.querySelector('button');
+
+//結果を出す関数
+var caluclateResult = () => {
+    var parentVal = document.getElementById('parent').value;
+    var requiredVal = document.getElementById('required').value;
+
+    if (!parentVal | !requiredVal) {
+        alert('Please fill in all fields.')
+    } else {
+        document.getElementById('result').innerHTML = requiredVal / parentVal;
+    }
+}
+
+button.addEventListener('click', caluclateResult);
+
+
+
